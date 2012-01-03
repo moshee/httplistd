fa27a1b3d3205b29ccc2691695007aee5e5aa110
moshee <teh.moshee@gmail.com>
2012-01-03 09:31:09 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+go-httplistd
+===
+
+What is it?
+---
+A simple and (imo) nice-looking HTTP server that lists and serves files and folders.
+
+Install
+---
+	$ gomake
+
+Make sure you have the Go weekly build. It won't compile otherwise.
+
+Use
+---
+	$ httplistd -help
+	Usage of httplistd:
+	  -listen=":8080": Port to listen on
+	  -root="/root/downloads": Base path to serve files from '/'
+The base path given by the `-root` option must be an absolute path if you use the equals sign (`-root=/home/user/sites/public`). If you must use shorthand, use a space (`-root ~/sites/public`).
+
+Once I figure out how to fork into the background, run httplistd with screen (`screen -S listd ./httplistd [OPTIONS]`) or jobs (`./httplistd [OPTIONS] &>/dev/null &`).
+
+For now it's a standalone executable. I may make it a package eventually.
+
+Suggestions, comments
+---
+Contact moshee on Freenode or Rizon.
---
+++ httplistd.go
@@ -0,0 +1,171 @@
+package main
+
+import (
+	"text/template"
+	"net/http"
+	"os"
+	"io/ioutil"
+	"fmt"
+	"flag"
+)
+
+var templateDirListing, templateNotFound *template.Template
+var basePath string
+
+type listing struct {
+	Path	string
+	Parent	string
+	Files	[]*listingEntry
+}
+
+type listingEntry struct {
+	IsDir		bool
+	IsLink		bool
+	Fullname	string
+	Basename	string
+	Size		string
+	Modified	string
+}
+
+const (
+	KB = 1024 << (10 * iota)
+	MB
+	GB
+	TB
+)
+
+func formatFilesize(n int64) string {
+	switch {
+	case n < KB:
+		return fmt.Sprintf("%d B", n)
+	case n < MB:
+		return fmt.Sprintf("%.2f KB", float64(n) / KB)
+	case n < GB:
+		return fmt.Sprintf("%.2f MB", float64(n) / MB)
+	case n < TB:
+		return fmt.Sprintf("%.2f GB", float64(n) / GB)
+	}
+	return fmt.Sprintf("%.2f TB", float64(n) / TB)
+}
+
+func getParentDir(requestPath string) (parentDir string) {
+	if requestPath != "/" {
+		// eat string up to the last forward slash
+		// unless it's the last character in the path
+		i := len(requestPath) - 1
+		index := i
+		for {
+			if requestPath[i] == '/' && i < index {
+				// include the slash, too
+				index = i + 1
+				break
+			}
+			i--
+		}
+		parentDir = requestPath[0:index]
+	}
+	return
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+//	if referer, ok := r.Header["Referer"]; ok {
+//		
+//	}
+	requestPath := r.URL.Path
+	completePath := basePath + requestPath
+	println(completePath)
+	file, err := os.Stat(completePath)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	if file.IsDir() {
+		// make sure all of our requestPaths have a / at the end
+		if requestPath[len(requestPath)-1] != '/' {
+			requestPath += "/"
+			completePath += "/"
+		}
+
+		// we don't want to show the actual root, the apparent root will actually be in the given basePath
+		dir, err := ioutil.ReadDir(completePath)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+
+		entries := make([]*listingEntry, len(dir))
+		for id, this := range dir {
+			thisFullPath := completePath + this.Name()
+
+			// if there exists an index.html in the current directory,
+			// serve it up instead of the listing
+			// TODO: Make this configurable?
+			if this.Name() == "index.html" {
+				http.ServeFile(w, r, thisFullPath)
+				return
+			}
+
+			isLink := this.Mode() & os.ModeSymlink != 0
+			isDir := this.IsDir()
+			// someone please look at this and help me DRY it up!
+			var size string
+			if isDir {
+				thisDirInfo, _ := ioutil.ReadDir(thisFullPath)
+				size = fmt.Sprintf("%v items", len(thisDirInfo))
+			} else if !isLink {
+				size = formatFilesize(this.Size())
+			} else {
+				thisLink, err := os.Stat(thisFullPath)
+				if err != nil {
+					size = "(invalid)"
+					break
+				}
+				if thisLink.IsDir() {
+					isDir = true
+					thisLinkInfo, _ := ioutil.ReadDir(thisFullPath)
+					size = fmt.Sprintf("%v items", len(thisLinkInfo))
+				} else {
+					size = formatFilesize(thisLink.Size())
+				}
+			}
+
+			entries[id] = &listingEntry{
+				IsDir:		isDir,
+				IsLink:		isLink,
+				Fullname:	requestPath + this.Name(),
+				Basename:	this.Name(),
+				Size:		size,
+				Modified:	this.ModTime().Format("02 Jan 2006 15:04"),
+			}
+		}
+
+		templateDirListing.Execute(w, &listing{requestPath, getParentDir(requestPath), entries})
+	} else {
+		http.ServeFile(w, r, completePath)
+	}
+}
+
+func main() {
+	var listenPort string
+	flag.StringVar(&listenPort, "listen", ":8080", "Port to listen on")
+	flag.StringVar(&basePath, "root", "/root/downloads", "Base path to serve files from '/'")
+	flag.Parse()
+
+	_, err := os.Open(basePath)
+	if err != nil {
+		panic(err)
+	}
+
+	templateDirListing, _ = template.ParseFiles("dir_listing.html.got")
+
+	println("Listening on port", listenPort)
+	http.HandleFunc("/", handler)
+	// for some reason can't get http.FileServer to work properly
+	// this probably isn't the best way to do this - if you have a folder called 'static'
+	// in basePath then the static handler will take precedence
+	http.HandleFunc("/static/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "." + r.URL.Path)
+	})
+	panic(http.ListenAndServe(listenPort, nil))
+}
